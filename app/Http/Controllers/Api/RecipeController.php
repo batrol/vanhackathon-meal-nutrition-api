09362e1c32d2997713f3fdf4d5a7b6af5c5a9ab6
Marcos <marcosgrimm@gmail.com>
2016-05-21 02:00:25 -0300

    Recipe namespace fix

--- app/Http/Controllers/Api/RecipeController.php
+++ app/Http/Controllers/Api/RecipeController.php
@@ -1,13 +1,18 @@
-<?php namespace App\Http\Controllers\Api;
+<?php
+
+namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use GoCanada\Models\Recipe;
+
 
 class RecipeController extends Controller
 {
     public function nutritionInfo($id)
     {
+        $Recipe = new Recipe();
 
-
+       return $Recipe->find($id);
         return [
             "nutrients" => [
                 [
