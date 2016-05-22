742e3c614704ff5298b974d84abc9ca1586e0471
batrol <plinioalmeida20@gmail.com>
2016-05-22 17:26:42 -0300

    fixes

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -48,6 +48,8 @@ class Handler extends ExceptionHandler
             $trace                 = $e->getTrace();
             $error['trace_last_5'] = array_slice($trace, 0, 5);
 
+            dd($trace);
+
             return response()->json($error, 500);
         }
 
--- config/app.php
+++ config/app.php
@@ -136,8 +136,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Way\Generators\GeneratorsServiceProvider::class,
-        Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider::class,
 
         /*
          * Application Service Providers...
