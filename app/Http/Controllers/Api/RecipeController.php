e1d951a0b3f5d3e127217b2786cc20ee0cd3a946
batrol <plinioalmeida20@gmail.com>
2016-05-22 16:54:34 -0300

    fixes

--- app/Http/Controllers/Api/RecipeController.php
+++ app/Http/Controllers/Api/RecipeController.php
@@ -27,7 +27,11 @@ class RecipeController extends Controller
 
     }
 
-    // Function responsible for giving Nutrient Information related to a identified Recipe.
+    /**
+     * @param $id
+     * @return JSON
+     * Function responsible for giving Nutrient Information related to an identified Recipe.
+     */
     public function nutritionInfo($id)
     {
         // Get all ingredients of the identified Recipe in the database.
@@ -35,28 +39,53 @@ class RecipeController extends Controller
         $recipe = new Recipe();
         $ingredients = $recipe->findOrFail($id)->ingredients;
 
+        //get the array with the nutrition info related to given ingredients
         $ingredientsNutritionInfo = $this->recipeRepo->sumNutritionInfo($ingredients, $this->ingredientsRepo);
 
         // return success with json of nutrients
         return $this->success(Response::HTTP_OK, null,['nutrients' => $ingredientsNutritionInfo]);
     }
 
+    /**
+     * @param Request $request
+     * @return JSON
+     * This function receives the recipe data and saves it to the DB. It returns the id of the inserted recipe.
+     */
     public function store(Request $request)
     {
+        //get a new recipe, fill it with the given information and stores it in the database
         $recipe = new Recipe();
         return $this->save($request, $recipe, "i");
     }
 
+    /**
+     * @param Request $request
+     * @return JSON
+     * This function receives the recipe data and updates it on the DB. It returns the id of the updated recipe.
+     */
     public function update(Request $request, $id)
     {
+        // Get the identified Recipe in the database.
+        // Also checks if there is no matching result for the $id and gives error response in that case.
         $recipe = Recipe::findOrFail($id);
 
+        //update the recipe in the database
         return $this->save($request, $recipe, "u");
     }
 
+    /**
+     * @param Request $request
+     * @param Recipe $recipe
+     * @param $action
+     * @return JSON
+     * This function receives the recipe data and saves it to the DB. It returns the id of the inserted/updated recipe.
+     */
     private function save(Request $request, Recipe $recipe, $action)
     {
+        //get all the post/put data
         $data = $request->all();
+
+        //validate if the given data match the requirements
         $rules = [
             'user_id' => 'required|integer|exists:user,id',
             'name' => 'required|unique:recipe,name,' . $recipe->id,
@@ -80,10 +109,12 @@ class RecipeController extends Controller
             return $this->error(Response::HTTP_BAD_REQUEST, implode(" ", $validator->errors()->all()), $validator->errors()->all());
         }
 
+        //get the array with the nutrition info related to given ingredients
         $ingredientsNutritionInfo = $this->recipeRepo->sumNutritionInfo($ingredientsPost, $this->ingredientsRepo);
 
-        //TODO: calculate the total_energy outside the transaction
+        //save the information in the DB
         DB::transaction(function () use($recipe, $ingredientsPost, $request, $ingredientsNutritionInfo) {
+            //store recipe information
             $recipe->user_id = $request->user_id;
             $recipe->name = $request->name;
             $recipe->visibility = $request->visibility;
@@ -91,19 +122,25 @@ class RecipeController extends Controller
 
             $recipe->save();
 
+            //insert, update and/or delete the ingredients related the recipe
             $ingredientsRecipe = [];
             $ingredients = $recipe->ingredients;
+            //check all the existent recipe's ingredients and update or delete them
             foreach ($ingredients as $ingredient) {
                 if (array_key_exists($ingredient->nbdno, $ingredientsPost)) {
+                    //update existent ingredients
                     $ingredient->quantity = $ingredientsPost[$ingredient->nbdno]->quantity;
                     $ingredient->save();
 
                     $ingredientsRecipe[] = $ingredient->ndbno;
                 }
                 else {
+                    //delete ingredients that were removed from the recipe
                     $ingredient->delete();
                 }
             }
+
+            //insert new recipe's ingredients
             foreach ($ingredientsPost as $ingredientPost){
                 if (!in_array($ingredientPost->ndbno, $ingredientsRecipe)) {
                     $ingredient = new IngredientRecipe();
@@ -115,16 +152,19 @@ class RecipeController extends Controller
             }
         });
 
+        //return the id of the new recipe
         if ($action == "i"){
             return $this->success(Response::HTTP_CREATED, "Recipe stored with id: {$recipe->id}!", ["id" => $recipe->id]);
         }
 
+        //return the id of the updated recipe
         return $this->success(Response::HTTP_OK, "Recipe with id {$recipe->id} updated!", ["id" => $recipe->id]);
     }
 
     /**
      * @param $name
-     * @return json
+     * @return JSON
+     * This function list all the recipes matching a name
      */
     public function searchByName($name)
     {
@@ -154,6 +194,11 @@ class RecipeController extends Controller
         return $this->success(Response::HTTP_OK, null, ["recipes" => $recipes]);
     }
 
+    /**
+     * @param $id
+     * @return JSON
+     * This function list all the recipes of an user
+     */
     public function searchByUser($id)
     {
 
@@ -181,6 +226,11 @@ class RecipeController extends Controller
 
     }
 
+    /**
+     * @param $min
+     * @return JSON
+     * This function list recipes with more than $min of energy (Kcal)
+     */
     public function searchByEnergyMin($min)
     {
 
@@ -206,6 +256,11 @@ class RecipeController extends Controller
         return $this->success(Response::HTTP_OK, null, ["recipes" => $recipes]);
     }
 
+    /**
+     * @param $max
+     * @return JSON
+     * This function list recipes with less than $max of energy (Kcal)
+     */
     public function searchByEnergyMax($max)
     {
 
@@ -230,6 +285,12 @@ class RecipeController extends Controller
         return $this->success(Response::HTTP_OK, null, ["recipes" => $recipes]);
     }
 
+    /**
+     * @param $min
+     * @param $max
+     * @return JSON
+     * This function list recipes by a range of energy (Kcal)
+     */
     public function searchByEnergyRange($min,$max)
     {
         ///set the input values
